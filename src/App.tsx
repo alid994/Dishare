d22427c269f9bf6e55d720a30830071440375161
alid994 <aljo_dozer@hotmail.com>
2024-01-13 12:55:05 +0100

    File & Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import './globals.css';
+import "./globals.css";
+import LoginForm from "./_auth/forms/LoginForm";
+import RegisterForm from "./_auth/forms/RegisterForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-    return (
-        <div>App</div>
-    )
-}
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/login" element={<RegisterForm />} />
+        </Route>
 
-export default App
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
